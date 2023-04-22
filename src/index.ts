264bb37481516be28dfd79a8476ce60e82c69692
ndeast <nik@ndeast.com>
2023-04-21 20:42:56 -0400

    update packages, and add lastfm service

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('hello world')
+import express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const API_KEY: string = process.env.API_KEY!
+const LASTFM_URL: string = "https://ws.audioscrobbler.com/2.0/"
+const USER_AGENT: string = 'merlin'
+
+
+const app = express();
+
+// super basic auth requirement. Should be expanded upon.
+app.use('', (req, res, next) => {
+  if (req.headers.authorization) {
+    next();
+  } else {
+    res.sendStatus(403)
+  }
+})
+
+app.get('/info', (req, res, next) => {
+  console.log('get');
+  return res.send("this is info")
+});
+
+app.use('/album-chart', (req, res, next) => {
+  return res.send("album chart")
+})
+// app.listen(3000)
---
+++ src/lastfm.ts
@@ -0,0 +1,17 @@
+export class lastFM {
+
+  API_KEY: string = process.env.API_KEY!
+  LASTFM_URL: string = "https://ws.audioscrobbler.com/2.0/"
+
+  getAlbumChart() {
+    let queryURL: string = this.LASTFM_URL + '?method=user.getweeklyalbumchart&user=ndeast&api_key=' + this.API_KEY + '&format=json'
+  }
+  
+
+
+
+
+
+
+
+}
