e3858993b0a64d8a369589c849a74eb86b7c4fa3
ndeast <nik@ndeast.com>
2023-04-22 22:55:33 -0400

    fix cors errors

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,31 @@
 import express from 'express';
 import "dotenv/config.js";
 import { lastFM } from './lastfm.ts';
+import cors from 'cors';
 
 
 const API_KEY: string = process.env.API_KEY!
 const LASTFM_URL: string = "https://ws.audioscrobbler.com/2.0/"
 const USER_AGENT: string = 'merlin'
 
+const allowedOrigins = ['http://localhost:4200']
+const corsOptions: cors.CorsOptions = {
+  origin: allowedOrigins
+}
 
 const app = express();
 
 const lfm = new lastFM();
 
+app.use(cors(corsOptions));
 // super basic auth requirement. Should be expanded upon.
 app.use('', (req, res, next) => {
   if (req.headers.authorization) {
-    next();
+       next();
   } else {
     res.sendStatus(403)
   }
-})
+  })
 
 app.get('/info', (req, res, next) => {
   console.log('get');
